2e05fe39faa092ce6549ba17c63485162c75c9b3
Mishok21 <mm21112000@gmail.com>
2026-01-14 21:53:24 +0300

    Adds handlebars

--- messenger/src/main.js
+++ messenger/src/main.js
@@ -3,23 +3,67 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import './styles/main.scss'
+import index from './templates/index.hbs'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+document.querySelector('#app').innerHTML = index(
+  {
+    chats: [
+      {
+        src: '#',
+        title: 'Андрей',
+        message: 'Изображение',
+        time: '10:49',
+        amount: '2',
+      },
+      {
+        src: '',
+        title: 'Киноклуб',
+        message: '<span>Вы:</span> стикер',
+        time: '12:00',
+        amount: '3',
+      },
+      {
+        // src: '#',
+        title: 'Илья',
+        message: 'Message',
+        time: '11:00',
+        amount: '3',
+      },
+      {
+        // src: '#',
+        title: 'Вадим',
+        message: 'Message2222',
+        time: '14:00',
+        amount: '1',
+      },
+      {
+        // src: '#',
+        title: 'Vadim',
+        message: 'Pupupu',
+        time: '21:20',
+        // amount: '',
+      }
+    ]
+  }
+);
+
 
 setupCounter(document.querySelector('#counter'))
--- messenger/vite.config.js
+++ messenger/vite.config.js
@@ -1,29 +1,7 @@
 import { defineConfig } from "vite";
-import handlebars from "vite-plugin-handlebars";
+// import handlebars from "vite-plugin-handlebars";
+import handlebars from "@yoichiro/vite-plugin-handlebars";
 
 export default defineConfig({
     plugins: [handlebars()],
-    // css: {
-    //     preprocessorOptions: {
-    //         scss: {
-    //             api: 'modern-compiler',
-    //         },
-    //     },
-    // },
-
-
-    // переменные/миксины во все scss без ручных импортов
-    // css: {
-    //     preprocessorOptions: {
-    //         scss: {
-    //             // additionalData: `@use "./src/styles/vars.scss" as *;`
-    //         },
-    //     },
-    // },
-
-    // build: {
-    //     outDir: 'build',
-    //     emptyOutDir: true,
-    //     assetsDir: 'assets',
-    // },
 })
